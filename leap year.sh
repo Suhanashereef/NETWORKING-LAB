6fe5a1c2e08c4a48da75b5d35651a7bcb7ea6809
SUHANA SHEREEF <159097906+Suhanashereef@users.noreply.github.com>
2024-03-14 15:04:58 +0530

    leap year.sh

---
+++ leap year.sh
@@ -0,0 +1,17 @@
+fi
+else
+echo "$year is a leap year"
+fi
+else
+echo "$year is not a leap year"
+fi
+
+ OUTPUT
+ --------
+mlm@mlm-OptiPlex-3020:~/Desktop/SUHANA/networking$ chmod +x leapyear.sh
+mlm@mlm-OptiPlex-3020:~/Desktop/SUHANA/networking$ ./leapyear.sh
+Enter the year (YYYY)
+2024
+2024 is a leap year
+mlm@mlm-OptiPlex-3020:~/Desktop/SUHANA/networking$ 
+
