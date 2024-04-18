7baf1a8311775452d1ea49db094e064227d461d5
SUHANA SHEREEF <159097906+Suhanashereef@users.noreply.github.com>
2024-04-18 15:12:47 +0530

    Create program29.sh

---
+++ program29.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Prompt the user for the string to check
+echo "Enter a string: "
+read string
+# Reverse the string
+reverse=$(echo $string | rev)
+# Check if the string is equal to its reverse
+if [ "$string" == "$reverse" ]; then
+echo "$string is a palindrome."
+else
+echo "$string is not a palindrome."
+fi
+
+output
+------
+mlm@mlm-OptiPlex-3020:~/Desktop/suhana/networking$ chmod +x program29.sh
+mlm@mlm-OptiPlex-3020:~/Desktop/suhana/networking$ ./program29.sh
+Enter a string: 
+malayalam
+malayalam is a palindrome.
