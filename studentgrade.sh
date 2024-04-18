d70676da5250ae411834381240765310b5caee79
SUHANA SHEREEF <159097906+Suhanashereef@users.noreply.github.com>
2024-04-18 13:58:57 +0530

    Create studentgrade.sh

---
+++ studentgrade.sh
@@ -0,0 +1,21 @@
+INPUT_FILE="grades.txt"
+if [[ ! -f "$INPUT_FILE" ]]; then
+echo "Input file not found!"
+exit 1
+fi
+declare -A grades
+while read line; do
+name=$(echo "$line" | cut -d ',' -f 1)
+grade=$(echo "$line" | cut -d ',' -f 2)
+grades["$name"]=$grade
+done < "$INPUT_FILE"
+for name in "${!grades[@]}"; do
+echo "$name: ${grades[$name]}"
+done
+
+output
+------
+aswin: 70
+suhana: 20
+krishna: 30
+megha: 80
